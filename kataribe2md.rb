e6d87987441422aed6999143c6e26aaf5c4df264
Yoshinori Kawasaki <ykawasaki@gmail.com>
2018-09-15 15:32:39 +0900

    Fix lint warning

--- kataribe2md.rb
+++ kataribe2md.rb
@@ -2,9 +2,9 @@
 
 txt = $stdin.read
 
-val = txt.split("\n\n").map { |txt|
-  header = txt.lines.first
-  body = txt.lines[1..-1]
+val = txt.split("\n\n").map { |block|
+  header = block.lines.first
+  body = block.lines[1..-1]
 
   ["\n\n### #{header}", "\n", "```\n", body, "\n```\n"].join
 }.join
